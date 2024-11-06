01b62b94eef05119b7317e18da2d8dfc498ffd5e
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2024-11-06 22:24:59 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+	return (
+		<div className="bg-slate-900 w-full h-full ">
+			<div>
+				<h1 className="text-white text-4xl text-center bg-slate-400">Hello, world!</h1>
+			</div>
+		</div>
+	);
+}
